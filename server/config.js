76d8f63e50f40a99f4499f831fddc66f21895a17
Revanth Nemani <revanth@vmi435929.contaboserver.net>
2020-08-24 11:46:54 +0200

    Initial commit

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  AUTH0_DOMAIN: "tailbuds.eu.auth0.com",
+  AUTH0_API_AUDIENCE: "http://localhost:8083/api/", // e.g., 'http://localhost:8083/api/'
+  MONGO_URI: "mongodb://[USER]:[PASSWORD]@[DS######].mlab.com:[PORT]/[DB_NAME]",
+};
